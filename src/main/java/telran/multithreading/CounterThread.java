da4e031916941a37447b5ba8927e1c98f65f794b
yanfi-sher <loss.soul20@gmail.com>
2024-11-19 19:24:43 +0200

    cw 42

--- src/main/java/telran/multithreading/CounterThread.java
+++ src/main/java/telran/multithreading/CounterThread.java
@@ -1,5 +1,27 @@
 package telran.multithreading;
 
-public class CounterThread {
+import java.util.concurrent.atomic.AtomicLong;
 
+public class CounterThread extends Thread{
+    private static  int nIterations;
+    private static AtomicLong counter = new AtomicLong();
+    public static int getnIterations() {
+        return nIterations;
+    }
+    public static void setnIterations(int nIterations) {
+        CounterThread.nIterations = nIterations;
+    }
+    public static long getCounter() {
+        return counter.get();
+    }
+   
+    private void counterIncrement() {
+        counter.incrementAndGet();
+    }
+    @Override
+    public void run() {
+        for(int i = 0; i < nIterations; i++) {
+            counterIncrement();
+        }
+    }
 }
